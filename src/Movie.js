7f1808c311c119c392fcefbfbcfd94bb0e153d4a
edc3665@gmail.com <edc3665@gmail.com>
2022-12-11 06:12:54 +0900

    complete

---
+++ src/Movie.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Movie.css"
+
+function Movie({year,title,summary,poster,genres}){
+    return(
+        <div className="movie">
+            <img src={poster} alt={title} title={title}></img>
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre,index) =>(
+                    <li key={index} className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie_summary">{summary.slice(0,140)}...</p>
+            </div>
+        </div>
+    );
+}
+
+export default Movie;
